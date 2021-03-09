4f30a4fb7a32634f542b8b3199762f1028f8fd52
Kosenko <Kosenko.2000@yandex.ru>
2021-03-09 23:27:33 +0500

    add

--- src/main/java/ru/geekbrains/lesson3/Lesson3Application.java
+++ src/main/java/ru/geekbrains/lesson3/Lesson3Application.java
@@ -1,7 +1,7 @@
 package ru.geekbrains.lesson3;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 public class Lesson3Application {
 
---
+++ src/main/java/ru/geekbrains/lesson3/config/AppConfig.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.lesson3.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+public class AppConfig implements WebMvcConfigurer {
+
+}
