f279a4dc89123a3f6134fe26bfd2ab24da2fc0b7
Kosenko <Kosenko.2000@yandex.ru>
2021-03-19 16:11:09 +0500

    4 lesson

--- src/main/java/ru/geekbrains/lesson3/Lesson3Application.java
+++ src/main/java/ru/geekbrains/lesson3/Lesson3Application.java
@@ -1,15 +1,9 @@
 package ru.geekbrains.lesson3;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import ru.geekbrains.lesson3.config.Cart;
-import ru.geekbrains.lesson3.config.Product;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.List;
 
+@SpringBootApplication
 public class Lesson3Application {
 
 	public static void main(String[] args) throws Exception {
